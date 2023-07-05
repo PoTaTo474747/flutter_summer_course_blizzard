26f0131c9c00e54a9914f8c0bfbd1e52c2a1194a
Blizzard-6 <blizzard-6@Admin-6-iMac.local>
2023-07-05 13:01:30 +0800

    day 08

--- day_08/lib/Guess_the_game.dart
+++ day_08/lib/Guess_the_game.dart
@@ -3,6 +3,16 @@ import 'dart:io';
 
 
     void main() {
-       random ();
-      int
+       final Random random = Random ();
+       final int randomNumber = random.nextInt(100);
+       print (randomNumber);
+       int attempts=0;
+       int guess=0;
+       print ('Welcome to Guess the Number!');
+       print ('I have chosen a number between 1 and 100.');
+       print ('Can you guess the number?');
+       while (randomNumber!=guess) {
+         stdout.write ('Enter ')
+       }
+
     }
