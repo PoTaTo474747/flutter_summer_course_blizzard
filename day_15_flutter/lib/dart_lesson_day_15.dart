badff8252a49fdb8fa4e9566f9d5898e4dec6f05
Blizzard-6 <blizzard-6@Admin-6-iMac.local>
2023-07-28 11:47:51 +0800

    add day_15_flutter

---
+++ day_15_flutter/lib/dart_lesson_day_15.dart
@@ -0,0 +1,9 @@
+var list = [1,3,4];
+
+
+
+var niilsenHomiYmaa= [5,6, ...list ];
+
+void main (){
+  print (list);
+}
---
+++ day_15_flutter/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'flex_screen.dart';
+
+void main() {
+  runApp(MaterialApp(home: FlexScreen()));
+}
