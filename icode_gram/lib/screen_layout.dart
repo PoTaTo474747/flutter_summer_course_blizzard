e5590ab14e009b489245a20c15b443f3cd8bc714
MUngunuu <you@example.com>
2023-08-25 10:37:32 +0800

    added: colors

--- icode_gram/lib/screen_layout.dart
+++ icode_gram/lib/screen_layout.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:icode_gram/screens/add_post_screen.dart';
 import 'package:icode_gram/screens/home_screen.dart';
+import 'package:icode_gram/screens/profile/profile_screen.dart';
 
 class ScreenLayout extends StatefulWidget {
   const ScreenLayout({super.key});
@@ -19,42 +21,44 @@ class _ScreenLayoutState extends State<ScreenLayout> {
     pageController = PageController();
   }
 
-  dispose() {
+  @override
+  void dispose() {
     pageController.dispose();
     super.dispose();
   }
 
-  void navigationTapped(int page) {
-    pageController.jumpToPage(page);
-  }
-
-  void onPageChanged(int page) {
-    setState(() {
-      _page = page;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
+    void navigationTapped(int page) {
+      pageController.jumpToPage(page);
+    }
+
+    void onPageChanged(int page) {
+      setState(() {
+        _page = page;
+      });
+    }
+
     return Scaffold(
       body: SafeArea(
         child: PageView(
           physics: NeverScrollableScrollPhysics(),
-          controller: PageController(),
+          controller: pageController,
           onPageChanged: onPageChanged,
           children: [
             HomeScreen(),
-            // AddPostScreen(),
-            // ProfileScreen(),
+            AddPostScreen(),
+            ProfileScreen(userName: '', ),
           ],
         ),
       ),
       bottomNavigationBar: CupertinoTabBar(
+        onTap: navigationTapped,
         items: [
           BottomNavigationBarItem(
             icon: Icon(
-              //
               Icons.home,
+              color: _page == 0 ? Colors.orange : Colors.black,
             ),
             label: '',
             backgroundColor: Colors.white,
@@ -62,21 +66,20 @@ class _ScreenLayoutState extends State<ScreenLayout> {
           BottomNavigationBarItem(
             icon: Icon(
               Icons.add_box_outlined,
+              color: _page == 1 ? Colors.orange : Colors.black,
             ),
             label: '',
             backgroundColor: Colors.white,
           ),
           BottomNavigationBarItem(
             icon: Icon(
-              Icons.home,
-
-              //
+              Icons.contact_mail_outlined,
+              color: _page == 2 ? Colors.orange : Colors.black,
             ),
             label: '',
             backgroundColor: Colors.white,
           ),
         ],
-        onTap: navigationTapped,
       ),
     );
   }
